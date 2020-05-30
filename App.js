0ddf4d839cb5b212dd2437aa4e1c7de03f043cca
fan-boy <aadityashete@gmail.com>
2020-05-31 00:36:37 +0530

    response added

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
 const http = require('http');
 
 const server = http.createServer(function(req,res){
-    console.log(req);
+    console.log(req.url,req.headers,req.method);
+    //process.exit(); to quit the server
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<body><h1>Hello,First Node Program</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000);
