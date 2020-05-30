c0535e6f94d0748755b4197c0a6660cae1768dda
fan-boy <aadityashete@gmail.com>
2020-05-31 01:16:28 +0530

    write to text file

--- App.js
+++ App.js
@@ -1,8 +1,36 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer(function(req,res){
-    console.log(req.url,req.headers,req.method);
+    //console.log(req.url,req.headers,req.method);
     //process.exit(); to quit the server
+    const url = req.url;
+    const method = req.method;
+    
+
+    if(url === "/"){
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<body><form action ="/message" method ="POST"><input type = "text" name="message"><button>Click Me!</buttton></body>');
+    res.write('</html>');
+    return res.end();
+    }
+    if(url === "/message" && method ==="POST"){
+        const body =[];
+        req.on('data',(chunk) =>{
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end',() =>{
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt',message);
+        });
+        
+        res.statusCode = 302;
+        res.setHeader('Location','/');
+        return res.end();
+    }
     res.setHeader('Content-Type','text/html');
     res.write('<html>');
     res.write('<body><h1>Hello,First Node Program</h1></body>');
